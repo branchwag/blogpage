c86288b3f1f2b265d3964126600a654c92939e0d
LeAnne B <branchleannelmb@gmail.com>
2023-06-29 09:36:26 -0400

    fixes the connection.js file to use JAWSDB and not just local dev

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,34 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// let sequelize;
+
+// are we in production?
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+// sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
